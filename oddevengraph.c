d2f049bc1996cde22773bb8a5ba824e48b2dbdbb
kuriki <kuriki@emobook-pro.local>
2018-02-20 10:57:57 +0900

    グラフ生成

---
+++ oddevengraph.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int GetRandom(int min,int max);
+
+int main(void){
+	int vid,i;
+	int edgenum = 10;
+	int size = 500000;
+	for(kivid = 0; vid < size*2; vid=vid+2){
+		printf("[%d,0,[",vid);
+		int rand[size];
+		for(i=0;i<edgenum;i++){
+			rand[i]=GetRandom(1,size)*2-1;
+			printf("[%d,0]",rand[i]);
+			if(i < edgenum - 1)
+			printf(",");
+		}
+		printf("]]\n");
+		for(i=0;i<edgenum;i++){
+			printf("[%d,0,[[%d,0]]]\n",rand[i],vid);
+		}
+	}
+return 0;
+}
+
+int GetRandom(int min,int max)
+{
+	return min + (int)(rand()*(max-min+1.0)/(1.0+RAND_MAX));
+}
